b06449381078afe333e541379a63870fa78d675e
Shriram Patil <shrirampatil246@gmail.com>
2023-02-13 19:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //your code here
-let textValue = document.getElementById("evaluatedText");
-let count = document.getElementById("letterCount");
+const express = require('express');
+const path = require('path');
 
-textValue.addEventListener("input", function() {
-	
-	count.innerText = this.value.length;
-	
-})
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
