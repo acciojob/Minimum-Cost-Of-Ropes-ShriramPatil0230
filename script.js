a2016b3e2454709c51c4f8beb53cde8ef043fddf
Shriram Patil <shrirampatil246@gmail.com>
2023-02-14 17:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //your code here
-
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
-module.exports = app;
